0dad630a57beea8f6def4bde551017fcf3228668
Om Sharma <omsharma.83173@gmail.com>
2025-05-10 02:56:06 +0530

    feat:intialized-http-layer

--- repo/apps/backend-server/src/index.ts
+++ repo/apps/backend-server/src/index.ts
@@ -1,1 +1,16 @@
-// All backend Stuff
+import express from "express";
+import cors from 'cors';
+import bodyParser from "body-parser";
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Backend Server Started! at PORT : ${PORT} `)
+})
