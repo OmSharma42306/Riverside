0235c4f390b661a92615d8919fcf8d40488f73dc
Om Sharma <omsharma.83173@gmail.com>
2025-05-10 03:03:54 +0530

    feat:initialized-websocket-server.

--- repo/apps/ws-layer/src/index.ts
+++ repo/apps/ws-layer/src/index.ts
@@ -1,1 +1,17 @@
 // Websocket layer.
+import {WebSocket,WebSocketServer} from "ws";
+
+const wss:WebSocket | any = new WebSocketServer({port:8080});
+
+
+wss.on('connection',function newConnection(ws:WebSocket){
+    console.log("New Connection!");
+    ws.on('error',console.error);
+
+    ws.on('message',function message(data:any){
+        const msg = JSON.parse(data);
+        console.log(msg);
+    })
+
+})
+
