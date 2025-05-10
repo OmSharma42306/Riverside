1798cc2eeeac27a8cdb690b03f541e36eed0de44
Om Sharma <omsharma.83173@gmail.com>
2025-05-10 17:50:25 +0530

    feat:added-offer-and-answer-creation-rtc-stuff.

--- repo/apps/frontend/src/components/Receiver.tsx
+++ repo/apps/frontend/src/components/Receiver.tsx
@@ -1,4 +1,48 @@
+import { useEffect, useState } from "react"
+
 export default function Receiver(){
+    const [socket,setSocket] = useState<WebSocket>();
+    
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8080');
+
+        socket.onopen = () =>{
+            console.log("Socket Connected!");
+            setSocket(socket);
+        }
+
+        let pc : RTCPeerConnection | null = null;
+        socket.onmessage = async (event:any) =>{
+            const msg = JSON.parse(event.data);
+            if(msg.type === "create-offer"){
+                pc = new RTCPeerConnection();
+                pc.setRemoteDescription(msg.sdp);
+                
+                
+        
+                // Create an Answer.
+                const answer = await pc.createAnswer();    
+                const sdp = await pc.setLocalDescription(answer);
+
+                socket.send(JSON.stringify({type:"create-answer",sdp:sdp}))
+        
+            }else if(msg.type === ""){
+
+            }
+        }
+        
+        
+
+        return () =>{
+            socket.close(); // Cleanup the Connection On Mount.
+        }
+        
+    },[socket])
+    
+    
+
+
     return <div>
         <h1>Hi from Receiver</h1>
     </div>
--- repo/apps/frontend/src/components/Sender.tsx
+++ repo/apps/frontend/src/components/Sender.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react"
+
 export default function Sender(){
+    const [socket,setSocket] = useState<WebSocket>();
+    const pc = new RTCPeerConnection();
+    
+    async function handleRtc(){
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+        socket?.send(JSON.stringify({type:"create-offer",sdp:offer}));
+    }
+    
+
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8080');
+
+        socket.onopen = () =>{
+            setSocket(socket);
+        }
+
+        socket.onmessage = (data:any) =>{
+            const msg = JSON.parse(data);
+            if(msg.type === "create-answer"){
+                pc.setRemoteDescription(msg.sdp);
+            }
+        }
+    },[])
+    
+    useEffect(()=>{
+        handleRtc()
+    socket?.send(JSON.stringify({msg:""}))
+    },[socket])
+    
+    
     return <div>
         <h1>Hi from Sender.</h1>
+
     </div>
 }
