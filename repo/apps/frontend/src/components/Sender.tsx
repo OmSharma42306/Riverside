3df385bd7459713fed692964feceeb1b61c0160b
Om Sharma <omsharma.83173@gmail.com>
2025-05-11 21:12:20 +0530

    feat:initial-sender-side-rtc-socket-stuff-done.

--- repo/apps/frontend/src/components/Sender.tsx
+++ repo/apps/frontend/src/components/Sender.tsx
@@ -2,39 +2,52 @@ import { useEffect, useState } from "react"
 
 export default function Sender(){
     const [socket,setSocket] = useState<WebSocket>();
+    const [roomId,setRoomId] = useState<string>("");
+
     const pc = new RTCPeerConnection();
     
     async function handleRtc(){
+        console.log("Called function for rtc");
         const offer = await pc.createOffer();
         await pc.setLocalDescription(offer);
         socket?.send(JSON.stringify({type:"create-offer",sdp:offer}));
     }
     
-
-
-    useEffect(()=>{
+    async function init(){
+    console.log("i am triggered!");
         const socket = new WebSocket('ws://localhost:8080');
 
         socket.onopen = () =>{
+            console.log("sockets connected!")
+            socket?.send(JSON.stringify({type:"sender",roomId:roomId}));
             setSocket(socket);
         }
 
-        socket.onmessage = (data:any) =>{
-            const msg = JSON.parse(data);
-            if(msg.type === "create-answer"){
+       
+        
+        
+        socket.onmessage = (event:any) =>{
+            console.log(event);
+            
+            const msg = JSON.parse(event.data);
+            console.log(msg)
+            if(msg.type === "receiver-remote-description"){
                 pc.setRemoteDescription(msg.sdp);
             }
         }
-    },[])
-    
-    useEffect(()=>{
-        handleRtc()
-    socket?.send(JSON.stringify({msg:""}))
-    },[socket])
-    
+
+    }
+
+    if(socket){
+        handleRtc();
+    }
     
     return <div>
         <h1>Hi from Sender.</h1>
+        <input type="text" placeholder="Enter RoomId" onChange={(e)=>{
+            setRoomId(e.target.value);
+        }} />
+        <button onClick={init}>Go</button>
 
     </div>
 }
