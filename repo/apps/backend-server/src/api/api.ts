9500e8d772d11580281b48d46cccc624d158e46a
Om Sharma <omsharma.83173@gmail.com>
2025-05-20 17:23:47 +0530

    feat:added-router-for-create-sessions-for-host.

--- repo/apps/backend-server/src/api/api.ts
+++ repo/apps/backend-server/src/api/api.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import userRouter from "../routes/user"
 import recordingRouter from "../routes/recordings";
-
+import sessionRouter from "../routes/host"
 const router = express.Router();
 
 router.use('/user',userRouter);
 router.use('/recordings',recordingRouter)
+router.use('/sessions',sessionRouter);
 
 export default router;
