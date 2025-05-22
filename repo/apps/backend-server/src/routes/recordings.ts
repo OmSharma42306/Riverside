7cd62982eb0ea52f7df8a7cc2e65694fc4addec8
Om Sharma <omsharma.83173@gmail.com>
2025-05-23 05:07:42 +0530

    feat: Implement-route-for-video-chunk-retrieval,-merge,-S3-upload

--- repo/apps/backend-server/src/routes/recordings.ts
+++ repo/apps/backend-server/src/routes/recordings.ts
@@ -3,6 +3,8 @@ import multer from "multer";
 import {S3Client,PutObjectCommand} from "@aws-sdk/client-s3"
 import { authMiddleware } from "../middlewares";
 import { prismaClient } from "@repo/db/prisma";
+import fs from "fs"
+import path from "path";
 
 const router = express.Router();
 
@@ -18,12 +20,12 @@ const s3 = new S3Client({
 
 
 interface fileType{
-    fieldname: string;
+    fieldname?: string;
     originalname: string;
-    encoding: string;
-    mimetype: string;
+    encoding?: string;
+    mimetype?: string;
     buffer: Buffer;
-    size: number;
+    size?: number;
 }
 
 interface authRequest extends Request{
@@ -51,6 +53,7 @@ async function uploadToS3(file:fileType){
 // For File Stuff..
 const upload = multer();
 
+// uploads a Complete Video File to S3.
 router.post('/upload-to-s3',upload.single('file'),authMiddleware,async(req:authRequest,res:Response)=>{
     // get video 
     const file = req.file;
@@ -100,7 +103,7 @@ router.post('/upload-to-s3',upload.single('file'),authMiddleware,async(req:authR
     }
 });
 
-
+// create the chunks and stores for temp storage in uploads folder by SessionId.
 router.post('/chunks',upload.single('chunk'),authMiddleware,async(req:authRequest,res:Response)=>{
     const file = req.file;
     const {chunkIndex,sessionName,sessionCode} = req.body;
@@ -108,15 +111,26 @@ router.post('/chunks',upload.single('chunk'),authMiddleware,async(req:authReques
     try{
         if(!file){
             res.status(400).json({msg:"File empty"})
-            return
+            return;
         }
+        if(!chunkIndex || !sessionCode || !sessionName){
+            res.status(400).json({msg:"Missing Data!"});
+        }
+
         
         // all chunks stuff
         console.log("ChunkIndex: ",chunkIndex);
         console.log("SessionName",sessionName);
         console.log("SessionCode",sessionCode);
         // store on local. after getting end chunk merge and upload to s3.
+        // Save each chunk to a temp directory
 
+        const dir = path.join(__dirname,'..','uploads','chunks',sessionName);
+        fs.mkdirSync(dir,{recursive:true})
+        
+        const chunkPath = path.join(dir,`${chunkIndex}.webm`);
+        fs.writeFileSync(chunkPath,file.buffer);
+        
         res.status(200).json({msg:"Success!",data:{chunkIndex,sessionCode,sessionName}});
         return;
     }catch(error){
@@ -124,5 +138,62 @@ router.post('/chunks',upload.single('chunk'),authMiddleware,async(req:authReques
         return;
     }
 })
+
+// merge the existing chunks and uploads to s3.
+router.post('/merge-upload-s3',authMiddleware,async (req:authRequest,res:Response)=>{
+    const sessionName = req.body.sessionId;
+    const dir = path.join(__dirname,'..','uploads','chunks',sessionName);
+
+    try{
+        if(!sessionName){
+            res.json(400).json({msg:"Empty Data! SessionId not Found!"});
+            return;
+        }
+        if(!dir){
+            res.status(400).json({msg:"Chunks Not Found!"});
+            return;
+        }
+
+        // sort the files of the existing chunks directory.ensures chunks are in correct numerical order.
+        const files = fs.readdirSync(dir).sort((a,b)=>
+            parseInt(a) - parseInt(b)
+        )
+
+        // the path where final merged video will be
+        const mergedPath = path.join(__dirname,'..','uploads',`${sessionName}.webm`);
+        // created a writeStream to write all chunks into single file to above mergedPath.
+        const writeStream = fs.createWriteStream(mergedPath);
+
+        // reading chunks 
+        for (const file of files){
+            const chunk = fs.readFileSync(path.join(dir,file));
+            writeStream.write(chunk);
+        }
+
+        writeStream.end();
+
+        // after writeStream end , the finish event will be called.
+        writeStream.on('finish',async()=>{
+            // so finalBuffer will be stored at mergedPath.Reading finalBuffer
+            const finalBuffer = fs.readFileSync(mergedPath);
+            // uploading that finalBuffer to S3 that is Final Merged Video.
+            const s3Url = await uploadToS3({buffer:finalBuffer,originalname:`${sessionName}.webm`});
+            // store metadata about this stuff to db.
+
+            // Removed Existing Unusable Chunks.
+            fs.rmSync(dir,{recursive:true,force:true});
+            fs.unlinkSync(mergedPath);
+            
+            // Sending an Downloadable url for user to Download Final Video.
+            res.status(200).json({msg:"Uploaded Successfully!",url:s3Url});
+        })
+
+        
+    }catch(error){
+        res.status(400).json({msg:error});
+        return;
+    }
+})
+
 
 export default router;
